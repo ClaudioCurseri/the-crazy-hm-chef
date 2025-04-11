06a629a4959cb505979897aafa2bd04e4c6ce86a
Claudio Curseri <curseri@hm.edu>
2025-04-11 10:28:22 +0200

    feat: add recipe model and recipe context with context provider

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,12 @@
 import { Stack } from 'expo-router';
+import {RecipeProvider} from "../context/RecipeContext";
 
 export default function RootLayout() {
     return (
-        <Stack>
-            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        </Stack>
+        <RecipeProvider>
+            <Stack>
+                <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+            </Stack>
+        </RecipeProvider>
     )
 }
---
+++ context/RecipeContext.tsx
@@ -0,0 +1,21 @@
+import React, {createContext, useContext} from "react";
+import {Recipe} from "../model/recipe";
+
+
+const recipes: Recipe[] = []
+
+const RecipeContext = createContext({
+    recipes,
+    getRecipeById: (id: string) => recipes.find(recipe => recipe.id === id)
+})
+
+export const useRecipes = () => useContext(RecipeContext);
+
+export function RecipeProvider({ children }: { children: React.ReactNode }) {
+    return (
+        <RecipeContext.Provider
+        value={useContext(RecipeContext)}>
+            {children}
+        </RecipeContext.Provider>
+    )
+}
